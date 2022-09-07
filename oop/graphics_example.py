874fbb0736c4b8af17bacf82c8bd77e8f0eb9233
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2022-09-07 20:32:11 +0200

    Create graphics_example.py

---
+++ oop/graphics_example.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+from py5 import Sketch
+import random, math
+
+class DrawableBase:
+    def __init__(self):
+        self.__stroke_color = [0, 0, 0]
+        self.__stroke_alpha = 255
+        self.__fill_color = [255, 255, 255]
+        self.__fill_alpha = 255
+        self.__stroke_width = 1
+
+    @property
+    def stroke_color(self):
+        return self.__stroke_color
+
+    @stroke_color.setter
+    def stroke_color(self, color):
+        self.__stroke_color = color
+
+    @property
+    def stroke_alpha(self):
+        return self.__stroke_alpha
+
+    @stroke_alpha.setter
+    def stroke_alpha(self, v):
+        self.__stroke_alpha = v
+
+    @property
+    def fill_color(self):
+        return self.__fill_color
+        
+    @fill_color.setter
+    def fill_color(self, color):
+        self.__fill_color = color
+
+    @property
+    def fill_alpha(self):
+        return self.__fill_alpha
+
+    @fill_alpha.setter
+    def fill_alpha(self, v):
+        self.__fill_alpha = v
+
+    @property
+    def stroke_width(self):
+        return self.__stroke_width
+
+    @stroke_width.setter
+    def stroke_width(self, width):
+        self.__stroke_width = width
+
+    def do_draw(self):
+        pass
+
+    def draw(self):
+        p5sketch.stroke(self.__stroke_color[0], self.__stroke_color[1], self.__stroke_color[2], self.__stroke_alpha)
+        p5sketch.fill(self.__fill_color[0], self.__fill_color[1], self.__fill_color[2], self.__fill_alpha)
+        p5sketch.stroke_weight(self.__stroke_width)
+
+        self.do_draw()
+        
+
+class Particle(DrawableBase):
+    def __init__(self, x=0.0, y=0.0):
+        super().__init__()
+        self.__x = x
+        self.__y = y
+        self.__vx = 0.0
+        self.__vy = 0.0
+
+        self.stroke_width = 2
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    @property
+    def vx(self):
+        return self.__vx
+
+    @vx.setter
+    def vx(self, v):
+        self.__vx = v
+
+    @property
+    def vy(self):
+        return self.__vy
+
+    @vy.setter
+    def vy(self, v):
+        self.__vy = v
+
+    def update(self, dt):
+        self.__x += self.__vx*dt
+        self.__y += self.__vy*dt
+
+    def do_draw(self):
+        p5sketch.point(self.x, self.y)
+
+class RoundParticle(Particle):
+    def __init__(self, x=0.0, y=0.0, r=1.0):
+        super().__init__(x, y)
+        self.__r = r
+
+    @property
+    def r(self):
+        return self.__r
+
+    @r.setter        
+    def r(self, r):
+        self.__r = r
+
+    def do_draw(self):
+        p5sketch.ellipse(self.x, self.y, self.r*2, self.r*2)
+
+class BoxBoundary:
+    def __init__(self):
+        self.__xmin = 0.0
+        self.__xmax = 600.0
+        self.__ymin = 0.0
+        self.__ymax = 600.0
+
+    def is_inside(self, p):
+        return (p.x - p.r > self.__xmin) and (p.x + p.r < self.__xmax) and (p.y - p.r > self.__ymin) and (p.y + p.r < self.__ymin)
+
+    def check(self, p):
+        if not self.is_inside(p):
+            if (p.x - p.r) < self.__xmin:
+                p.vx = -p.vx
+                p.x = self.__xmin + p.r
+            if (p.x + p.r) > self.__xmax:
+                p.vx = -p.vx
+                p.x = self.__xmax - p.r
+            if (p.y - p.r) < self.__ymin:
+                p.vy = -p.vy
+                p.y = self.__ymin + p.r
+            if (p.y + p.r) > self.__ymax:
+                p.vy = -p.vy
+                p.y = self.__ymax - p.r
+
+
+class ParticleSketch(Sketch):
+
+    def settings(self):
+        self.size(600, 600)
+
+    def setup(self):
+
+        self.particles = []
+        self.boundary = BoxBoundary()
+        self.ellipse_mode(self.CENTER)
+
+        for i in range(100):
+            p = RoundParticle(self.random(0,600), self.random(0, 600), self.random(30, 70))
+            p.vx = self.random(-60.0, 60.0)
+            p.vy = self.random(-60.0, 60.0)
+            p.fill_color = [self.random(255), self.random(255), self.random(255)]
+            p.fill_alpha = self.random(50, 255)
+            self.particles.append(p)
+
+    def draw(self):
+        self.background(40)
+
+        for p in self.particles:
+            p.update(1.0/60.0)
+            self.boundary.check(p)
+            p.draw()
+
+if __name__ == "__main__":
+
+    global p5sketch
+    p5sketch = ParticleSketch()
+    p5sketch.run_sketch()        
