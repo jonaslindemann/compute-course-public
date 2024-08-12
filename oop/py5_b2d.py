79737f1bf9c996d7913a0980353a0f2474cb7246
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2024-08-12 21:39:10 +0200

    Update py5_b2d.py

--- oop/py5_b2d.py
+++ oop/py5_b2d.py
@@ -4,53 +4,70 @@ from py5 import Sketch
 from py5base import *
 import random, math
 
+import numpy as np
+
 import Box2D  # The main library
 # Box2D.b2 maps Box2D.b2Vec2 to vec2 (and so on)
 from Box2D.b2 import (world, polygonShape, circleShape, staticBody, dynamicBody)
 
+global sketch
+global g_world
+
+PPM = 20.0  # pixels per meter
+TARGET_FPS = 60
+TIME_STEP = 1.0 / TARGET_FPS
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+
 class DynamicBody(DrawableBase):
-    def __init__(self, world, ppm=20.0):
-        self.__world = world
+    def __init__(self, body=None, fixture=None):
         self.__body = None
         self.__fixture = None
         self.__ppm = 20.0
 
     @property
-    def world(self):
-        return self.__world
-    
-    @world.setter
-    def world(self, value):
-        self.__world = value
-
-    @property
     def body(self):
         return self.__body
+    
+    @body.setter
+    def body(self, value):
+        self.__body = value
     
     @property
     def fixture(self):
         return self.__fixture
     
-    @property
-    def ppm(self):
-        return self.__ppm
+    @fixture.setter
+    def fixture(self, value):
+        self.__fixture = value
     
-    @ppm.setter
-    def ppm(self, value):
-        self.__ppm = value
 
-    def do_draw(self):
-        position = self.__body.transform * self.__fixture.pos * self.__ppm
+class DynamicPolygon(DynamicBody):
+    def __init__(self, body, fixture):
+        super().__ini__(body, fixture)
+
+    def on_draw(self):
+        vertices = [(self.body.transform * v) * PPM for v in self.fixture.vertices]
+        vertices = [(v[0], SCREEN_HEIGHT - v[1]) for v in self.fixture.vertices]
+        sketch.points()
+        pygame.draw.polygon(screen, colors[body.type], vertices)
+
+class DynamicCircle(DynamicBody):
+    def __init__(self, body, fixture):
+        super().__ini__(body, fixture)
+
+    def on_draw(self):
+        pass
+
+
+
+
+
 
 
 
                 
 class Box2DSketch(Sketch):
 
-    PPM = 20.0  # pixels per meter
-    TARGET_FPS = 60
-    TIME_STEP = 1.0 / TARGET_FPS
-    SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 
     def settings(self):
         self.size(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
@@ -60,16 +77,16 @@ class Box2DSketch(Sketch):
 
         # --- pybox2d world setup ---
         # Create the world
-        self.world = world(gravity=(0, -10), doSleep=True)
+        g_world = world(gravity=(0, -10), doSleep=True)
 
         # And a static body to hold the ground shape
-        self.ground_body = world.CreateStaticBody(
+        self.ground_body = g_world.CreateStaticBody(
             position=(0, 0),
             shapes=polygonShape(box=(50, 1)),
         )
 
         # Create a couple dynamic bodies
-        body = world.CreateDynamicBody(position=(20, 45))
+        body = g_world.CreateDynamicBody(position=(20, 45))
         self.circle = body.CreateCircleFixture(radius=0.5, density=1, friction=0.3)
 
         body = world.CreateDynamicBody(position=(30, 45), angle=15)
@@ -83,6 +100,5 @@ class Box2DSketch(Sketch):
 
 if __name__ == "__main__":
 
-    app = SketchApp()
-    app.sketch = Box2DSketch()
-    app.run()        
+    sketch = ParticleSketch()
+    sketch.run_sketch()        
